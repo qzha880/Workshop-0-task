4f65fdb3c93e2c826860b81db07de9d396250bc8
qzha880 <qzha880@aucklanduni.ac.nz>
2025-01-15 22:19:23 +0800

    Made some changes to the sketch
    * colour
    * add lines
    * background colour
    * delete unnecessary code

--- sketch.js
+++ sketch.js
@@ -7,41 +7,37 @@ function setup() {
 }
 
 function draw() {
-  background(220);
-
-  stroke(20, 50, 50);
-  fill(0, 225, 0, 100);
-  ellipseMode(CORNER);
+  background(250, 200, 200);
+  
+  stroke(0, 60, 255, 100);
+  line(0, 600, 600, 0);
+  
+  
+  strokeWeight(5)
+  stroke(255);
+  fill(255, 255, 0);
   square(x, height / 2 - 50, 100);
 
-  x = x + y;
-  if (x > width - 100) {
-    y = -3;
-  }
-  if (x < 0) {
-    y = 3;
-  }
+  stroke(255, 255, 0, 100);
+  line(0, 300, x, 300);
+  line(600, 300, x + 100, 300);
 
-  stroke(50, 20, 50);
-  fill(225, 0, 0, 100);
+  stroke(255);
+  fill(225, 0, 0);
   ellipseMode(CORNER);
   circle(x, x, 100);
 
-  x = x + y;
-  if (x > height - 100) {
-    y = -3;
-  }
-  if (x < 0) {
-    y = 3;
-  }
+  stroke(225, 0, 0, 100);
+  line(0, 0, x + 12, x + 12);
+  line(600, 600, x + 88, x + 88);
 
-  stroke(50, 20, 50);
-  fill(0, 0, 225, 100);
-  ellipseMode(CORNER);
+  stroke(255);
+  fill(0, 60, 255);
   triangle(x, height + z, x + 50, height + z - 100, x + 100, height + z);
 
   x = x + y;
   z = -x;
+  
   if (x > width - 100) {
     y = -3;
   }
